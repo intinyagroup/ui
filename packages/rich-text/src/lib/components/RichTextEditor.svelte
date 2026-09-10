2129bddd12374cbf0cc5a77975b02330652c4aa0
Antonius Joshua <antoniusjoshua47@gmail.com>
2026-09-10 15:04:27 +0700

    fix(rich-text): use named TipTap v3 Table export

--- packages/rich-text/src/lib/components/RichTextEditor.svelte
+++ packages/rich-text/src/lib/components/RichTextEditor.svelte
@@ -10,7 +10,7 @@
   import Link from "@tiptap/extension-link";
   import Image from "@tiptap/extension-image";
   import Highlight from "@tiptap/extension-highlight";
-  import Table from "@tiptap/extension-table";
+  import { Table } from "@tiptap/extension-table";
   import TableRow from "@tiptap/extension-table-row";
   import TableCell from "@tiptap/extension-table-cell";
   import TableHeader from "@tiptap/extension-table-header";
